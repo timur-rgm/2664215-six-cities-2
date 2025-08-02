719382759bf44da243a339c01497f47718b8cde9
Timur <Timur>
2025-08-02 12:27:15 +0500

    2.5. Add mock server data type

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,3 +1,4 @@
 export * from './comment.type.js';
+export * from './mock-server-data.type.js';
 export * from './offer.type.js';
 export * from './user.type.js';
---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,10 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  cities: string[];
+  previewImages: string[];
+  images: string[];
+  types: string[];
+  amenities: string[];
+  hosts: string[];
+};
