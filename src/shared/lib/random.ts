013cc8e9adb235f3b60ab30f60ac3db0c6b246ae
Timur <Timur>
2025-08-02 14:17:53 +0500

    2.8. Add getRandomBoolean helper

--- src/shared/lib/random.ts
+++ src/shared/lib/random.ts
@@ -1,17 +1,19 @@
-export const generateRandomValue = (min: number, max: number, numAfterDigit = 0): number =>
+export const getRandomNumber = (min: number, max: number, numAfterDigit = 0): number =>
   Number(((Math.random() * (max - min)) + min).toFixed(numAfterDigit));
 
+export const getRandomBoolean = (): boolean => Math.random() < 0.5;
+
 export const getRandomArrayItem = <T>(arr: T[]): T => {
-  const randomIndex = generateRandomValue(0, arr.length - 1);
+  const randomIndex = getRandomNumber(0, arr.length - 1);
   return arr[randomIndex];
 };
 
 export const getRandomArrayItems = <T>(items: T[]): T[] => {
   const itemsCopy = [...items];
-  const length = generateRandomValue(1, itemsCopy.length);
+  const length = getRandomNumber(1, itemsCopy.length);
 
   return Array.from({ length }, () => {
-    const index = generateRandomValue(0, itemsCopy.length - 1);
+    const index = getRandomNumber(0, itemsCopy.length - 1);
     return itemsCopy.splice(index, 1)[0];
   });
-}
+};
