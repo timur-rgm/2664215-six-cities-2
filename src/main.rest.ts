81cc87269e2627ebc7d57cb398cfe0fa4c731e59
Timur <Timur>
2025-08-09 17:57:33 +0500

    3.11. Add RestConfig to RestApplication

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,10 +1,12 @@
 import { PinoLogger } from './shared/libs/logger/index.js';
+import { RestConfig } from './shared/libs/config/index.js';
 import { RestApplication } from './rest/index.js';
 
 const bootstrap = async () => {
   const logger = new PinoLogger();
+  const config = new RestConfig(logger);
 
-  const application = new RestApplication(logger);
+  const application = new RestApplication(config,logger);
   await application.init();
 };
 
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,9 +1,14 @@
+import type{ Config } from '../shared/libs/config/index.js';
 import type { Logger } from '../shared/libs/logger/index.js';
 
 export class RestApplication {
-  constructor(private readonly logger: Logger) {}
+  constructor(
+    private readonly config: Config,
+    private readonly logger: Logger,
+  ) {}
 
   public async init() {
     this.logger.info('Application initialization');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
