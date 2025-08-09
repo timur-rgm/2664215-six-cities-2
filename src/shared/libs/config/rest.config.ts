f705d5fa6620ed767dfe795c5e143864647517bb
Timur <Timur>
2025-08-09 17:47:25 +0500

    3.10. Add dotenv to RestConfig

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,7 +1,21 @@
-import { Config } from './config.interface.js';
+import { config } from 'dotenv';
+import type { DotenvParseOutput} from 'dotenv';
+import type { Config } from './config.interface.js';
+import type { Logger } from '../logger/index.js';
 
 export class RestConfig implements Config {
-  constructor(private readonly config: NodeJS.ProcessEnv) {}
+  private readonly config: NodeJS.ProcessEnv;
+
+  constructor(private readonly logger: Logger) {
+    const dotenvConfig = config();
+
+    if (dotenvConfig.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
+    }
+
+    this.config = dotenvConfig.parsed as DotenvParseOutput;
+    this.logger.info('.env file found and successfully parsed!');
+  }
 
   public get(key: string): string | undefined {
     return this.config[key];
