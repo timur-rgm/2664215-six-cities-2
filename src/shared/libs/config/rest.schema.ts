90153052b73010d9120e03bf31e96c268972141f
Timur <Timur>
2025-08-10 10:49:34 +0500

    3.16. Extend restConfigSchema

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -1,14 +1,31 @@
 import convict from 'convict';
+import validator from 'convict-format-with-validator';
+
+convict.addFormats(validator);
 
 export type RestSchema = {
+  DB_HOST: string;
   PORT: number;
+  SALT: string;
 }
 
 export const restConfigSchema = convict<RestSchema>({
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
+  },
   PORT: {
     doc: 'Port for incoming connections',
     format: 'port',
     env: 'PORT',
     default: 3000
+  },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: null
   },
 });
