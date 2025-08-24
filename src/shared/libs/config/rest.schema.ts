c4e868a9e42f5eb2fb99ee29423b9130d231e2af
Timur <Timur>
2025-08-24 14:30:26 +0500

    4.3. Add db env vars

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -5,6 +5,9 @@ convict.addFormats(validator);
 
 export type RestSchema = {
   DB_HOST: string;
+  DB_PORT: string;
+  DB_USER: string;
+  DB_PASSWORD: string;
   PORT: number;
   SALT: string;
 }
@@ -15,6 +18,24 @@ export const restConfigSchema = convict<RestSchema>({
     format: 'ipaddress',
     env: 'DB_HOST',
     default: '127.0.0.1'
+  },
+  DB_USER: {
+    doc: 'Username to connect to the database',
+    format: String,
+    env: 'DB_USER',
+    default: null,
+  },
+  DB_PASSWORD: {
+    doc: 'Password to connect to the database',
+    format: String,
+    env: 'DB_PASSWORD',
+    default: null,
+  },
+  DB_PORT: {
+    doc: 'Port to connect to the database (MongoDB)',
+    format: 'port',
+    env: 'DB_PORT',
+    default: '27017',
   },
   PORT: {
     doc: 'Port for incoming connections',
