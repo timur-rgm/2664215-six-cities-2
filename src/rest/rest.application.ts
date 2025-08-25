e44731076a0fb071f473bafbfaafff3e29dfa55b
Timur <Timur>
2025-08-25 19:49:01 +0500

    4.8. Add init database connection

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,17 +1,35 @@
 import { injectable, inject } from 'inversify';
-import type { Config, RestSchema } from '../shared/libs/config/index.js';
-import type { Logger } from '../shared/libs/logger/index.js';
+
 import { Component } from '../shared/types/index.js';
+import type { Config, RestSchema } from '../shared/libs/config/index.js';
+import type { DatabaseClient } from '../shared/libs/database-client/index.js';
+import type { Logger } from '../shared/libs/logger/index.js';
+import { getMongoURI } from '../shared/helpers/index.js';
 
 @injectable()
 export class RestApplication {
   constructor(
     @inject(Component.Config) private readonly config: Config<RestSchema>,
+    @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
     @inject(Component.Logger) private readonly logger: Logger
   ) {}
 
+  private async initDB() {
+    const mongoURI = getMongoURI(
+      this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME')
+    );
+
+    await this.databaseClient.connect(mongoURI);
+  }
+
   public async init() {
     this.logger.info('Application initialization');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+    this.logger.info('Initializing database...');
+    await this.initDB();
+    this.logger.info('Database initialized');
   }
 }
--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -4,21 +4,16 @@ import validator from 'convict-format-with-validator';
 convict.addFormats(validator);
 
 export type RestSchema = {
-  DB_HOST: string;
-  DB_PORT: string;
   DB_USER: string;
   DB_PASSWORD: string;
+  DB_HOST: string;
+  DB_PORT: string;
+  DB_NAME: string;
   PORT: number;
   SALT: string;
 }
 
 export const restConfigSchema = convict<RestSchema>({
-  DB_HOST: {
-    doc: 'IP address of the database server (MongoDB)',
-    format: 'ipaddress',
-    env: 'DB_HOST',
-    default: '127.0.0.1'
-  },
   DB_USER: {
     doc: 'Username to connect to the database',
     format: String,
@@ -31,11 +26,23 @@ export const restConfigSchema = convict<RestSchema>({
     env: 'DB_PASSWORD',
     default: null,
   },
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
+  },
   DB_PORT: {
     doc: 'Port to connect to the database (MongoDB)',
     format: 'port',
     env: 'DB_PORT',
     default: '27017',
+  },
+  DB_NAME: {
+    doc: 'Database name (MongoDB)',
+    format: String,
+    env: 'DB_NAME',
+    default: 'six-cities'
   },
   PORT: {
     doc: 'Port for incoming connections',
