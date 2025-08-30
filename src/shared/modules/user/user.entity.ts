539a9148733603e4e2703b51b9345f11d328cb93
Timur <Timur>
2025-08-30 18:47:52 +0500

    4.14. Add id field to UserEntity

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,6 +1,10 @@
 import { prop, defaultClasses, getModelForClass } from '@typegoose/typegoose';
 import type { UserType, UserRole } from '../../types/index.js';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface UserEntity extends defaultClasses.Base {}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements UserType {
   @prop({ required: true })
   public name: string;
