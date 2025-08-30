f9db2bdbee88dad7de3b5a49eec3287b97127a6b
Timur <Timur>
2025-08-30 19:50:38 +0500

    4.18. Add CreateUserDto

---
+++ src/shared/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,9 @@
+import type { UserRole } from '../../../types/index.js';
+
+export class CreateUserDto {
+  public name: string;
+  public email: string;
+  public password: string;
+  public avatarUrl: string;
+  public type: UserRole;
+}
