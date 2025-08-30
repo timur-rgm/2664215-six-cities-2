3396dbcf230b3df0da69cc7eef3e6676a53bf006
Timur <Timur>
2025-08-30 12:33:06 +0500

    4.12. Add UserModel

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -1,1 +1,1 @@
-export { UserEntity } from './user.entity.js';
+export * from './user.entity.js';
--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
+import { prop, getModelForClass } from '@typegoose/typegoose';
 import type { UserType, UserRole } from '../../types/index.js';
 
 export class UserEntity implements UserType {
+  @prop({ required: true })
   public name: string;
+
+  @prop({ required: true, unique: true })
   public email: string;
-  public avatarUrl: string;
+
+  @prop()
+  public avatarUrl?: string;
+
+  @prop({ required: true })
   public password: string;
+
+  @prop({ required: true, type: () => String })
   public type: UserRole;
 }
+
+export const UserModel = getModelForClass(UserEntity);
