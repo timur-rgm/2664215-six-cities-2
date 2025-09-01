c8b6930c4b322ee3982a94941a86b9f04bd85de8
Timur <Timur>
2025-09-01 12:03:59 +0500

    4.22. Inject Logger into DefaultUserService

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -1,15 +1,26 @@
+import { inject, injectable } from 'inversify';
 import type { DocumentType } from '@typegoose/typegoose';
+
 import type { UserService } from './user-service.interface.js';
 import { UserEntity, UserModel } from './user.entity.js';
 import { CreateUserDto } from './dto/index.js';
+import { Component } from '../../types/index.js';
+import type { Logger } from '../../libs/logger/index.js';
 
+@injectable()
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 export class DefaultUserService implements UserService {
+  constructor(
+    @inject(Component.Logger) private readonly logger: Logger
+  ) {}
+
   public async create(userData: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(userData);
     user.setPassword(userData.password, salt);
 
-    return UserModel.create(user);
+    const result = await UserModel.create(user);
+    this.logger.info(`New user created: ${user.email}`);
+    return result;
   }
 }
