f84e0ee202efa3e1867a74fe3ffa22ec2c8693c2
Timur <Timur>
2025-09-01 11:43:58 +0500

    4.21. Add rest application container

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -2,39 +2,16 @@ import 'reflect-metadata';
 import { Container } from 'inversify';
 
 import { Component } from './shared/types/index.js';
-import { MongoDatabaseClient } from './shared/libs/database-client/index.js';
-import { PinoLogger } from './shared/libs/logger/index.js';
-import { RestConfig } from './shared/libs/config/index.js';
 import { RestApplication } from './rest/index.js';
-import type { DatabaseClient } from './shared/libs/database-client/index.js';
-import type { Config, RestSchema } from './shared/libs/config/index.js';
-import type { Logger } from './shared/libs/logger/index.js';
+import { createRestApplicationContainer } from './rest/rest.container.js';
 import { getErrorMessage } from './shared/helpers/index.js';
 
 const bootstrap = async () => {
-  const container = new Container();
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const appContainer = Container.merge(createRestApplicationContainer());
 
-  container
-    .bind<RestApplication>(Component.RestApplication)
-    .to(RestApplication)
-    .inSingletonScope();
-
-  container
-    .bind<Config<RestSchema>>(Component.Config)
-    .to(RestConfig)
-    .inSingletonScope();
-
-  container
-    .bind<Logger>(Component.Logger)
-    .to(PinoLogger)
-    .inSingletonScope();
-
-  container
-    .bind<DatabaseClient>(Component.DatabaseClient)
-    .to(MongoDatabaseClient)
-    .inSingletonScope();
-
-  const application = container.get<RestApplication>(Component.RestApplication);
+  const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
 };
 
