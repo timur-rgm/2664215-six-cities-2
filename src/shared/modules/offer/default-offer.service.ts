05ac0ae90058e5acbf7171045b0c7995ff8c3b94
Timur <Timur>
2025-09-02 12:08:36 +0500

    4.29. Add DefaultOfferService, implement create method

---
+++ src/shared/modules/offer/default-offer.service.ts
@@ -0,0 +1,22 @@
+import { inject } from 'inversify';
+import { types } from '@typegoose/typegoose';
+import type { DocumentType } from '@typegoose/typegoose';
+
+import { Component } from '../../types/index.js';
+import type { OfferService } from './offer-service.interface.js';
+import type { Logger } from '../../libs/logger/index.js';
+import { OfferEntity } from './offer.entity.js';
+import { CreateOfferDto } from './dto/index.js';
+
+export class DefaultOfferService implements OfferService {
+  constructor(
+    @inject(Component.Logger) private readonly logger: Logger,
+    @inject(Component.OfferModel) private readonly offerModel: types.ModelType<OfferEntity>,
+  ) {}
+
+  public async create(offerData: CreateOfferDto): Promise<DocumentType<OfferEntity>> {
+    const result = await this.offerModel.create(offerData);
+    this.logger.info(`New offer created: ${offerData.title}`);
+    return result;
+  }
+}
