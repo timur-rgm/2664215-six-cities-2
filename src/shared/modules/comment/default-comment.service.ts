644ba002c95ecbf320ff9b2be706897550f0292c
Timur <Timur>
2025-11-10 21:19:36 +0400

    5.2.14. Add automatic offer rating recalculation on new comment

--- src/shared/modules/comment/default-comment.service.ts
+++ src/shared/modules/comment/default-comment.service.ts
@@ -3,6 +3,7 @@ import { DocumentType, types } from '@typegoose/typegoose';
 
 import { CommentService } from './comment-service.interface.js';
 import { CommentEntity } from './comment.entity.js';
+import { OfferService } from '../offer/index.js';
 import { CreateCommentDto } from './dto/index.js';
 import { Component } from '../../types/index.js';
 
@@ -10,10 +11,36 @@ import { Component } from '../../types/index.js';
 export class DefaultCommentService implements CommentService {
   constructor(
     @inject(Component.CommentModel) private readonly commentModel: types.ModelType<CommentEntity>,
+    @inject(Component.OfferService) private readonly offerService: OfferService,
   ) {}
 
   public async createComment(commentData: CreateCommentDto): Promise<DocumentType<CommentEntity>> {
     const comment = await this.commentModel.create(commentData);
+    const { offerId } = comment;
+
+    const [{ averageRating }] = await this.commentModel
+      .aggregate([
+        {
+          $match: { offerId }
+        },
+        {
+          $group: { _id: null, averageRating: { $avg: '$rating' } }
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'userId',
+            foreignField: '_id',
+            as: 'userId'
+          }
+        },
+        {
+          $unwind: '$userId'
+        },
+      ])
+      .exec();
+
+    await this.offerService.updateById(String(offerId), { rating: averageRating });
     return comment.populate('userId');
   }
 
