ba4b3f9033eecf96a4ad7b1411e945919e5fdf80
Timur <Timur>
2025-11-29 14:46:30 +0400

    6.9. Register offer controller in RestApplication

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -4,6 +4,7 @@ import { inject, injectable } from 'inversify';
 
 import { Component } from '../shared/types/index.js';
 import type { Config, RestSchema } from '../shared/libs/config/index.js';
+import type { Controller } from '../shared/libs/rest/index.js';
 import type { DatabaseClient } from '../shared/libs/database-client/index.js';
 import type { Logger } from '../shared/libs/logger/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
@@ -15,6 +16,7 @@ export class RestApplication {
   constructor(
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
+    @inject(Component.OfferController) private readonly offersController: Controller,
     @inject(Component.Logger) private readonly logger: Logger,
   ) {
     this.server = express();
@@ -37,11 +39,19 @@ export class RestApplication {
     await this.databaseClient.connect(mongoURI);
   }
 
+  private initControllers() {
+    this.server.use('/offers', this.offersController.router);
+  }
+
   public async init() {
     this.logger.info('Application initialization');
     this.logger.info('Initializing database...');
     await this.initDB();
     this.logger.info('Database initialized');
+
+    this.logger.info('Init controllers');
+    this.initControllers();
+    this.logger.info('Controller initialization completed');
 
     this.logger.info('Try to init server…');
     this.initServer();
