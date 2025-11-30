86700b1535841b75d98ac0a6f1f9493474dec1d7
Timur <Timur>
2025-11-30 10:37:28 +0400

    6.10. Implement offers controller index handler

--- src/shared/modules/offer/offer.controller.ts
+++ src/shared/modules/offer/offer.controller.ts
@@ -1,14 +1,16 @@
 import { inject, injectable } from 'inversify';
-import { type Request, type Response } from 'express';
+import type { Request, Response } from 'express';
 
 import { BaseController, HttpMethod } from '../../libs/rest/index.js';
 import { Component } from '../../types/index.js';
-import { type Logger } from '../../libs/logger/index.js';
+import type { Logger } from '../../libs/logger/index.js';
+import type { OfferService } from './offer-service.interface.js';
 
 @injectable()
 export class OfferController extends BaseController {
   constructor(
-    @inject(Component.Logger) protected readonly logger: Logger
+    @inject(Component.Logger) protected readonly logger: Logger,
+    @inject(Component.OfferService) private readonly offerService: OfferService
   ) {
     super(logger);
 
@@ -18,10 +20,13 @@ export class OfferController extends BaseController {
     this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
   }
 
-  public index(req: Request, res: Response): void {
-    // Код обработчика
+  public async index(_req: Request, res: Response): Promise<void> {
+    const offers = await this.offerService.findAll();
+    this.ok(res, offers);
   }
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   public create(req: Request, res: Response): void {
     // Код обработчика
   }
