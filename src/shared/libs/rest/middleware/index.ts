0808db66e25319c2de8c2f19be6038b6baff064b
Timur <Timur>
2026-01-01 12:51:59 +0400

    7.2.9. Add UploadFileMiddleware

--- src/shared/libs/rest/middleware/index.ts
+++ src/shared/libs/rest/middleware/index.ts
@@ -2,3 +2,4 @@ export * from './document-exists.middleware.js';
 export * from './middleware.interface.js';
 export * from './validate-dto.middleware.js';
 export * from './validate-mongo-object-id.middleware.js';
+export * from './upload-file.middleware.js';
