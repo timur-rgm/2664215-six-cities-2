f5904fa9d26cc8bbc7511fe87fd7851d415a06a4
Timur <Timur>
2026-01-14 11:51:25 +0400

    8.11. Extend Express Request type

---
+++ custom.d.ts
@@ -0,0 +1,7 @@
+import type { TokenPayload } from './src/shared/modules/auth/index.js';
+
+declare module 'express-serve-static-core' {
+  export interface Request {
+    tokenPayload: TokenPayload;
+  }
+}
