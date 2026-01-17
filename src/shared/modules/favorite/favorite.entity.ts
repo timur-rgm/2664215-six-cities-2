81aa2f822c5d240eb53a3e1e5890fa12aec89588
Timur <Timur>
2026-01-17 11:52:57 +0400

    8.20. Add favorite model and entity

---
+++ src/shared/modules/favorite/favorite.entity.ts
@@ -0,0 +1,36 @@
+import {
+  defaultClasses,
+  getModelForClass,
+  modelOptions,
+  prop,
+  type Ref
+} from '@typegoose/typegoose';
+import { UserEntity } from '../user/index.js';
+import { OfferEntity } from '../offer/index.js';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface FavoriteEntity extends defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'favorites',
+    timestamps: true,
+  }
+})
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export class FavoriteEntity extends defaultClasses.TimeStamps {
+  @prop({
+    ref: UserEntity,
+    required: true,
+  })
+  public userId: Ref<UserEntity>;
+
+
+  @prop({
+    ref: OfferEntity,
+    required: true,
+  })
+  public offerId: Ref<OfferEntity>;
+}
+
+export const FavoriteModel = getModelForClass(FavoriteEntity);
