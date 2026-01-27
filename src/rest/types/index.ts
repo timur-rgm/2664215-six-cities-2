6050e3286a147bcde0fe6c22ac4981494182f608
Timur <Timur>
2026-01-27 17:48:20 +0400

    9.1.1. Add reduceValidationErrors

---
+++ src/rest/types/index.ts
@@ -0,0 +1,1 @@
+export * from './validation-error-field.type.js';
--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -1,5 +1,7 @@
-import { plainToInstance } from 'class-transformer';
-import type { ClassConstructor } from 'class-transformer';
+import { plainToInstance, type ClassConstructor } from 'class-transformer';
+import { ValidationError } from 'class-validator';
+
+import type { ValidationErrorField } from '../../rest/types/index.js';
 
 export const getErrorMessage = (error: unknown): string | null => {
   if (error instanceof Error) {
@@ -31,3 +33,11 @@ export const parseBooleanString = (value?: string): boolean | undefined => {
   }
 };
 
+export const reduceValidationErrors = (
+  errors: ValidationError[]
+): ValidationErrorField[] =>
+  errors.map(({ property, value, constraints}) => ({
+    property,
+    value,
+    messages : constraints ? Object.values(constraints) : []
+  }));
