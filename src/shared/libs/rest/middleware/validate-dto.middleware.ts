d1644777b57b011a5c251133b531c2a36b94ef0a
Timur <Timur>
2026-01-27 18:09:16 +0400

    9.1.2. Add ValidationError

--- src/shared/libs/rest/middleware/validate-dto.middleware.ts
+++ src/shared/libs/rest/middleware/validate-dto.middleware.ts
@@ -1,25 +1,25 @@
-import { validate } from 'class-validator';
 import { plainToInstance, type ClassConstructor } from 'class-transformer';
-import { StatusCodes } from 'http-status-codes';
+import { validate } from 'class-validator';
 import type { Request, Response, NextFunction } from 'express';
 
+import { reduceValidationErrors } from '../../../helpers/index.js';
+import { ValidationError } from '../../../../rest/errors/index.js';
 import type { Middleware } from './middleware.interface.js';
 
 export class ValidateDtoMiddleware implements Middleware {
   constructor(private dtoClass: ClassConstructor<object>) {}
 
-  public async execute(req: Request, res: Response, next: NextFunction) {
-    const { body } = req;
+  public async execute(req: Request, _res: Response, next: NextFunction) {
+    const { body, path } = req;
 
     const dtoInstance = plainToInstance(this.dtoClass, body);
     const errors = await validate(dtoInstance);
 
     if (errors.length > 0) {
-      res
-        .status(StatusCodes.BAD_REQUEST)
-        .send(errors);
-
-      return;
+      throw new ValidationError(
+        `Validation error: ${path}`,
+        reduceValidationErrors(errors)
+      );
     }
 
     next();
